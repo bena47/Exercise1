74a96663c2a5b6d53819d795cdbe3b3770cbb591
bena47 <bernard.njuguna@strathmore.edu>
2024-11-01 10:41:01 +0300

    added methods under class department

--- system.py
+++ system.py
@@ -1,2 +1,17 @@
 from abc import ABC, abstractmethod
 class Department:
+      def __init__(self, name):
+        self.__name = name
+        self.__employees = []
+        def get_name(self):
+            return self.__name
+
+        def add_employee(self, employee):
+            self.__employees.append(employee)
+
+        def remove_employee(self, employee):
+            if employee in self.__employees:
+                self.__employees.remove(employee)
+
+        def get_employees(self):
+            return self.__employees  
